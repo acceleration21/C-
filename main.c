b1a65ebc369ad0c7041c3b28dbfc02accaeaf3a5
wangjunze <171635094@qq.com>
2019-01-16 15:57:08 +0800

    基本框架

--- main.c
+++ main.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
+while(1){
+    printf("1--添加字符\n");
+    printf("2--删除最后一个字符\n");
+    printf("3--显示全部字符\n");
+    printf("4--查找替换敏感字符\n");
+    printf("5--退出\n");
 
-    printf("hello world!");
+    printf("输入相应的功能编码\n");
+    int code;
+    scanf("%d",&code);
+    
+    if (code == 1) {
+        /* code */
+    }
+    if (code == 2) {
+        /* code */
+    }
+    if (code == 3) {
+        /* code */
+    }
+    if (code == 4) {
+        /* code */
+    }
+    if (code == 5) {
+        printf("回车退出\n");
+        break;
+    }
 
+
+}
     return 0;
-
 }
